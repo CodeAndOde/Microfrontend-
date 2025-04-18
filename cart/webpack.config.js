170586fa94c00e5ba6bb48c99bd435af00163c6e
JerwinJustin <jerwin.justin@paarabit.com>
2025-04-18 08:25:58 -0400

    Added webpackconfig

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -7,6 +7,15 @@ devServer:{
     port:8082
 },
 plugins:[
-
+ new  ModuleFederationPlugin({
+name:'cart',
+filename:'remoteEntry.js',
+exposes:{
+    './CartShow': './src/index'
+}
+ }),
+ new HtmlWebpackPlugin({
+    template: './public/index.html'
+ })
 ],
 }
