e4b57d3b5468416c3cc4f3f318b9dc3919d71b40
JerwinJustin <jerwin.justin@paarabit.com>
2025-04-18 08:23:04 -0400

    added the index.html and index.js file for cart  product

---
+++ cart/webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports ={
+mode:'development',
+devServer:{
+    port:8082
+},
+plugins:[
+
+],
+}
